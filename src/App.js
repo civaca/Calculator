56d791b2fe6a189439933e5d600d47258589421e
Cristian Vaca <civaca7@gmail.com>
2023-01-31 23:19:31 -0500

    Almost done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import './App.css';
-import { showValue } from './redux/calcSlice';
+import { reset, showExpre, addExpre,resetViewNum,changeViewNum} from './redux/calcSlice';
+
 
 
 function App() {
   const dispatch=useDispatch();
   const value=useSelector((state)=>state.calc.value)
+  const newValue=useSelector((state)=>state.calc.newExpre)
+  const newNumber=useSelector((state)=>state.calc.viewNum)
+  const newDisplay=useSelector((state)=>state.calc.showDisplay)
+  const Parser = require('expr-eval').Parser;
+  
+ 
+  
+  const parser = new Parser();
+    
 
-  let handleClick=()=>{
-    dispatch(showValue("Dios es bueno"))
-    console.log(value)
-  }
+
+  
+
+ let addToExpre=(x)=>{
+  
+  
+  dispatch(changeViewNum(x))
+
+  
+ }
+ 
+ let addToExpre2=(x)=>{
+
+ 
+
+  dispatch(changeViewNum(x));
+  dispatch(resetViewNum(x))
+
+  
+ }
+
+ let showResult=()=>{
+  dispatch(resetViewNum());
+  ;
+  dispatch(showExpre())
+  
+  
+ }
 
   return (
     <div className="App">
-      <header className="App-header">
+      
         <h1>Calculator</h1>
-        <button onClick={handleClick}>Sum 1</button>
-        <h2>{value}</h2>
-        <p>Loja</p>
-      </header>
+        <span  id="display"  >
+        
+        <h2>{newDisplay}</h2>
+        </span>
+        
+        <div >
+        
+        <p>{value}</p>
+        <p>{newValue}</p>
+        <p>{newNumber}</p>
+          
+        </div>
+      <div className='App-main'>
+          <button id="zero" onClick={()=>addToExpre("0")}>0</button>
+          <button id="one" onClick={()=>addToExpre("1")}>1</button>
+          <button id="two" onClick={()=>addToExpre("2")}>2</button>
+          <button id="three" onClick={()=>addToExpre("3")}>3</button>
+          <button id="four" onClick={()=>addToExpre("4")}>4</button>
+          <button id="five" onClick={()=>addToExpre("5")}>5</button>
+          <button id="six" onClick={()=>addToExpre("6")}>6</button>
+          <button id="seven" onClick={()=>addToExpre("7")}>7</button>
+          <button id="eight" onClick={()=>addToExpre("8")}>8</button>
+          <button id="nine" onClick={()=>addToExpre("9")}>9</button>
+        <button id ="add" onClick={()=>addToExpre2("+")}>+</button>
+        <button id="subtract" onClick={()=>addToExpre2("-")}>-</button>
+        <button id ="divide" onClick={()=>addToExpre2("/")}>/</button>
+        <button id="multiply" onClick={()=>addToExpre2("*")}>X</button>
+        <button id="decimal" onClick={()=>addToExpre(".")}>.</button>
+        <button id="equals"onClick={showResult}>=</button>
+        <button id="clear"onClick={()=>dispatch(reset())}>Reset</button>
+        </div>
     </div>
   );
 }
--- src/redux/calcSlice.js
+++ src/redux/calcSlice.js
@@ -1,15 +1,67 @@
 import { createSlice } from "@reduxjs/toolkit";
+const Parser = require('expr-eval').Parser;
 
 export const calcSlice = createSlice({
 name:'calculator',
-initialState:{value:"example"},
+initialState:{
+    value:0,
+    newExpre:"",
+    viewNum:"",
+    showDisplay:"0"
+},
 reducers:{
-    showValue:(state,action)=>{
-        state.value=action.payload
-        console.log(state);
+    reset:(state)=>{
+        state.value=0;
+        state.newExpre="";
+        state.viewNum=""
+        state.showDisplay="0"
+    },
+    showExpre:(state,action)=>{
+        state.value=Parser.evaluate(state.viewNum)
+        state.showDisplay=state.value
+        state.newExpre=state.value
+        state.viewNum="";
+        
+    },
+    addExpre:(state, action)=>{
+        
+       
+        state.newExpre+=action.payload
+        
+        
+       
+            
+        
+    },
+    changeViewNum:(state,action)=>{
+        let regex=/^0|\+|\-|\/|\*/;
+        let reg1=/\./
+
+        
+        
+        if(regex.test(state.showDisplay)){
+            state.showDisplay=state.showDisplay.slice(1)
+        }
+       
+        if((reg1.test(state.showDisplay) && reg1.test(action.payload)) ){
+            state.showDisplay=state.showDisplay
+        } else{state.showDisplay+=action.payload}
+
+      
+        
+        
+    },
+    resetViewNum:(state,action)=>{
+        let x=state.viewNum.length;
+        let refinal=/\+|\-|\/|\*/
+        let reg2=/(?<=(\+|\-|\/|\*))\+|\-|\/|\*/
+      
+        state.viewNum+=state.showDisplay;
+        state.showDisplay=action.payload
+        
     }
 },
 }); 
 
-export const {showValue}=calcSlice.actions;
+export const {reset, showExpre, addExpre, changeViewNum,resetViewNum}=calcSlice.actions;
 export default calcSlice.reducer;
